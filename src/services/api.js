f26f7e9ebfd25c4db092fe4f0ad4eb353f61cb74
Aldemir Gomes <aldemir.gomes@MOFAGROUP.local>
2019-06-19 16:28:22 -0300

    mudando backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://omnistack-backender.herokuapp.com/',
+    baseURL: 'https://omnistack-backender.herokuapp.com',
 })
 
 export default api;
